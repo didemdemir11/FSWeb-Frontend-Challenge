a35c40fd0e4be7bd92ecfe11efc6e333690ac680
didemdemir11 <kdidemdemir@gmail.com>
2024-10-08 19:01:40 +0300

    projects css update - rounded box

--- dd-frontend-project/src/components/projects.jsx
+++ dd-frontend-project/src/components/projects.jsx
@@ -8,22 +8,25 @@ const Projects = () => {
           <h2 className="text-4xl md:text-5xl lg:text-[48px] font-bold mb-12 text-customPurple">
             Projects
           </h2>
-          <div className="bg-white rounded-[12px]">
-            <div>
-              <img src="/images/projects-img-1.png" />
+          <div className="bg-white rounded-[18px] flex flex-col md:flex-row items-stretch ">
+            <div className="flex justify-center items-center mx-auto- max-w-[360px] md:max-h-[360px]">
+              <img
+                src="/images/projects-img-1.png"
+                className="w-full h-full object-cover rounded-t-[18px]  md:rounded-l-[18px] md:rounded-r-none"
+              />
             </div>
-            <div>
+            <div className="flex-1 p-6">
               <h3 className="text-2xl font-bold text-customPurple">
                 Workintech
               </h3>
-              <p className="text-[#383838]">
+              <p className="text-[#383838] mt-4">
                 A simple, customizable, minimal setup cookie plugin that allows
                 your users to select which cookies to accept or decline. This
                 was created with vanilla JS, SCSS and Parcel Bundler and is
                 available as a NPM package and the git repository makes any type
                 of customization to code and themes possible.
               </p>
-              <div>
+              <div className="mt-4 space-x-2">
                 <button className="bg-customPurple text-white rounded px-4">
                   react
                 </button>
@@ -34,7 +37,7 @@ const Projects = () => {
                   vercel
                 </button>
               </div>
-              <div>
+              <div className="mt-4 space-x-2">
                 <a
                   href="https://www.linkedin.com/in/didem-demir-a37917116/"
                   className="text-black font-bold underline"
@@ -50,44 +53,47 @@ const Projects = () => {
               </div>
             </div>
           </div>
-          <div className="bg-white  mt-8 rounded-[12px]">
-            <div>
-              <img src="/images/projects-img-2.png" />
+          <div className="bg-white rounded-[18px] flex flex-col md:flex-row items-stretch mt-4 ">
+            <div className="flex justify-center items-center mx-auto- max-w-[360px] md:max-h-[360px]">
+              <img
+                src="/images/projects-img-2.png"
+                className="w-full h-full object-cover rounded-t-[18px]  md:rounded-l-[18px] md:rounded-r-none"
+              />
             </div>
-            <div>
+            <div className="flex-1 p-6">
               <h3 className="text-2xl font-bold text-customPurple">Journey</h3>
-              <p className="text-[#383838]">
+              <p className="text-[#383838] mt-4">
                 A simple, customizable, minimal setup cookie plugin that allows
                 your users to select which cookies to accept or decline. This
                 was created with vanilla JS, SCSS and Parcel Bundler and is
                 available as a NPM package and the git repository makes any type
                 of customization to code and themes possible.
               </p>
-            </div>
-            <div>
-              <button className="bg-customPurple text-white rounded px-4">
-                react
-              </button>
-              <button className="bg-customPurple text-white rounded px-4">
-                redux
-              </button>
-              <button className="bg-customPurple text-white rounded px-4">
-                vercel
-              </button>
-            </div>
-            <div>
-              <a
-                href="https://www.linkedin.com/in/didem-demir-a37917116/"
-                className="text-black font-bold underline"
-              >
-                View Site
-              </a>
-              <a
-                href="https://www.linkedin.com/in/didem-demir-a37917116/"
-                className="text-black font-bold underline"
-              >
-                GitHub
-              </a>
+              <div className="mt-4 space-x-2">
+                <button className="bg-customPurple text-white rounded px-4">
+                  react
+                </button>
+                <button className="bg-customPurple text-white rounded px-4">
+                  redux
+                </button>
+                <button className="bg-customPurple text-white rounded px-4">
+                  vercel
+                </button>
+              </div>
+              <div className="mt-4 space-x-2">
+                <a
+                  href="https://www.linkedin.com/in/didem-demir-a37917116/"
+                  className="text-black font-bold underline"
+                >
+                  View Site
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/didem-demir-a37917116/"
+                  className="text-black font-bold underline"
+                >
+                  GitHub
+                </a>
+              </div>
             </div>
           </div>
         </section>
