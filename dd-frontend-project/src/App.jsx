9d9f1c6ca409d3c6b7c3f87e99aa9404e9d60d67
didemdemir11 <kdidemdemir@gmail.com>
2024-10-08 21:32:10 +0300

    creating LanguageProvider w/ useContext and apply for app

--- dd-frontend-project/src/App.jsx
+++ dd-frontend-project/src/App.jsx
@@ -1,21 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Hero from "./components/hero";
 import Skills from "./components/skills";
 import Profile from "./components/profile";
 import Projects from "./components/projects";
 import Footer from "./components/footer";
+import data from "./data/data.json";
+import { LanguageProvider } from "./contexts/LanguageContext";
 
 function App() {
+  const [language, setLanguage] = useState("eng");
+  const toggleLanguage = () => {
+    setLanguage((prevLang) => (prevLang === "tr" ? "eng" : "tr"));
+  };
   return (
     <>
-      <div className="App">
-        <Hero />
-        <Skills />
-        <Profile />
-        <Projects />
-        <Footer />
-      </div>
+      <LanguageProvider value={{ language, toggleLanguage }}>
+        <div className="App">
+          <Hero data={data.hero} />
+          <Skills data={data.skills} />
+          <Profile data={data.profile} />
+          <Projects data={data.projects} />
+          <Footer data={data.footer} />
+        </div>
+      </LanguageProvider>
     </>
   );
 }
