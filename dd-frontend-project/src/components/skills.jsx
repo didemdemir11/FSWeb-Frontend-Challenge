6ce9402178b8037b7f66152439ceacd651e4b590
didemdemir11 <kdidemdemir@gmail.com>
2024-10-08 16:42:15 +0300

    skills page css update

--- dd-frontend-project/src/components/skills.jsx
+++ dd-frontend-project/src/components/skills.jsx
@@ -1,59 +1,60 @@
 import React from "react";
 const Skills = () => {
   return (
-    <section className="pt-[100px] pb-[100px] px-6 md:px-8 lg:px-60">
-      <h2 className="text-4xl md:text-5xl lg:text-[48px] font-bold mb-12 text-customPurple">
-        Skills
-      </h2>
-      <div className="flex flex-wrap justify-end mt-[100px]">
-        <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+    <section className="pb-[100px] px-6 md:px-8 lg:px-60">
+      <div className="flex justify-between mt-[100px]">
+        <h2 className="text-4xl md:text-5xl lg:text-[48px] font-bold mb-12 text-customPurple mr-20 ">
+          Skills
+        </h2>
+        <ul className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-2 gap-y-8 flex-grow">
           <li className="flex items-center space-x-4">
             <img
               src="/logos/jslogo.jpeg"
               alt="JavaScript Logo"
-              className="w-[80px] h-[80px] md:w-[120px] md:h-[120px]"
+              className="w-[80px] h-[80px] md:w-[120px] md:h-[120px] rounded-[6px] shadow-lg"
             />
-            <span>JAVASCRIPT</span>
-          </li>
-          <li className="flex items-center space-x-4">
-            <img
-              src="/logos/react-logo.png"
-              alt=""
-              className="w-[80px] h-[80px] md:w-[120px] md:h-[120px]"
-            />
-            <span>REACT</span>
-          </li>
-          <li className="flex items-center space-x-4">
-            <img
-              src="/logos/reduxlogo.png"
-              alt=""
-              className="w-[80px] h-[80px] md:w-[120px] md:h-[120px]"
-            />
-            <span>REDUX</span>
+            <span className="text-[#777777] font-medium">JAVASCRIPT</span>
           </li>
           <li className="flex items-center space-x-4">
             <img
               src="/logos/node-logo.jpeg"
-              alt=""
-              className="w-[80px] h-[80px] md:w-[120px] md:h-[120px]"
+              alt="Node logo"
+              className="w-[80px] h-[80px] md:w-[120px] md:h-[120px] rounded-[6px] shadow-lg"
             />
-            <span>NODE</span>
+            <span className="text-[#777777] font-medium">NODE</span>
+          </li>
+          <li className="flex items-center space-x-4">
+            <img
+              src="/logos/react-logo.png"
+              alt="React Logo"
+              className="w-[80px] h-[80px] md:w-[120px] md:h-[120px] rounded-[6px] shadow-lg"
+            />
+            <span className="text-[#777777] font-medium">REACT</span>
           </li>
           <li className="flex items-center space-x-4">
             <img
               src="/logos/vscode-logo.png"
-              alt=""
-              className="w-[80px] h-[80px] md:w-[120px] md:h-[120px]"
+              alt="Vs Code Logo"
+              className="w-[80px] h-[80px] md:w-[120px] md:h-[120px] rounded-[6px] shadow-lg"
             />
-            <span>VS CODE</span>
+            <span className="text-[#777777] font-medium">VS CODE</span>
           </li>
           <li className="flex items-center space-x-4">
             <img
-              src="/logos/figma-logo.jpeg"
-              alt=""
-              className="w-[80px] h-[80px] md:w-[120px] md:h-[120px]"
+              src="/logos/reduxlogo.png"
+              alt="REDUX Logo"
+              className="w-[80px] h-[80px] md:w-[120px] md:h-[120px] rounded-[6px] shadow-lg"
             />
-            <span>FIGMA</span>
+            <span className="text-[#777777] font-medium">REDUX</span>
+          </li>
+
+          <li className="flex items-center space-x-4">
+            <img
+              src="/logos/figma-logo.jpeg"
+              alt="Figma Logo"
+              className="w-[80px] h-[80px] md:w-[120px] md:h-[120px] rounded-[6px] shadow-lg"
+            />
+            <span className="text-[#777777] font-medium">FIGMA</span>
           </li>
         </ul>
       </div>
