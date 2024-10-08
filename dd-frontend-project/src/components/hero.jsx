2a5086c5aa365ff765c8413ce224e777d3dcded6
didemdemir11 <kdidemdemir@gmail.com>
2024-10-08 21:40:15 +0300

    apply language context for hero component

--- dd-frontend-project/src/components/hero.jsx
+++ dd-frontend-project/src/components/hero.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
 import GitHubButton from "react-github-btn";
+import { LanguageContext } from "../contexts/LanguageContext";
 
-const Hero = () => {
+const Hero = ({ data }) => {
+  const { language, toggleLanguage } = useContext(LanguageContext);
   return (
     <section
       className="hero content w-full h-full"
@@ -12,11 +14,16 @@ const Hero = () => {
       <div className="pt-[100px] pb-[100px] px-6 md:px-8 lg:px-60">
         <header className="flex justify-between items-center relative">
           <h1 className="text-2xl md:text-3xl font-bold text-customGreen self-center ">
-            didem
+            {data.header.name}
           </h1>
           <div className="flex space-x-4 self-start">
-            <button className="text-customGreen font-bold ">
-              TÜRKÇE'YE GEÇ
+            <button
+              className="text-customGreen font-bold"
+              onClick={toggleLanguage}
+            >
+              {language === "tr"
+                ? data.buttons.language_switch_eng
+                : data.buttons.language_switch_tr}
             </button>
             <div>
               <button>
@@ -24,33 +31,35 @@ const Hero = () => {
                   <div className="h-4 w-4 rounded-full bg-[#FFE86E] shadow-sm ring-1 ring-slate-700/10 transition duration-200 ease-in-out translate-x-4"></div>
                 </div>
               </button>
-              <span className="text-customPurple font-bold">DARK MODE</span>
+              <span className="text-customPurple font-bold">
+                {data.buttons.dark_mode}
+              </span>
             </div>
           </div>
         </header>
         <main className="flex flex-col lg:flex-row justify-between items-center mt-8">
           <div className="flex flex-col items-start">
             <h1 className="mb-4 text-customGreen font-bold text-5xl">
-              I am a Frontend Developer...
+              {language === "tr"
+                ? data.content.title_tr
+                : data.content.title_eng}
             </h1>
             <p className="mb-4 text-white font-normal">
-              ...who likes to craft solid and scalable frontend products with
-              great user experiences.
+              {language === "tr"
+                ? data.content.description_tr
+                : data.content.description_eng}
             </p>
 
             <div className="links flex space-x-4">
               <GitHubButton
-                href="https://github.com/didemdemir11"
+                href={data.links.github}
                 data-color-scheme="no-preference: light; light: dark; dark: light;"
                 data-size="large"
                 aria-label="Follow @buttons on GitHub"
               >
                 Github
               </GitHubButton>
-              <a
-                href="https://www.linkedin.com/in/didem-demir-a37917116/"
-                className="text-white font-bold"
-              >
+              <a href={data.links.linkedin} className="text-white font-bold">
                 LinkedIn
               </a>
             </div>
@@ -58,7 +67,7 @@ const Hero = () => {
 
           <div className="flex justify-center items-center mx-auto- max-w-[350px] md:max-h-[350px]">
             <img
-              src="https://bit.ly/4dxOncn"
+              src={data.images.hero_image}
               className="w-full h-auto object-cover rounded-[18px]"
             />
           </div>
