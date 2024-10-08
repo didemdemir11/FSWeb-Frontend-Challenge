93e0f40b3e0b492fe760975629a29e853337d5ac
didemdemir11 <kdidemdemir@gmail.com>
2024-10-08 21:54:28 +0300

    language context apply for profile component

--- dd-frontend-project/src/components/profile.jsx
+++ dd-frontend-project/src/components/profile.jsx
@@ -1,64 +1,83 @@
-import React from "react";
+import React, { useContext } from "react";
+import { LanguageContext } from "../contexts/LanguageContext";
 
-const Profile = () => {
+const Profile = ({ data }) => {
+  const { language } = useContext(LanguageContext);
   return (
     <>
       <div className="bg-customPurple">
         <section className="pt-[100px] pb-[100px] px-6 md:px-8 lg:px-60">
           <h2 className="text-4xl md:text-5xl lg:text-[48px] font-bold mb-12 text-customGreen">
-            Profile
+            {language === "tr" ? data.title_tr : data.title_eng}
           </h2>
 
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
             <div className="flex flex-col justify between space-y-6 flex-1 ">
               <h3 className="text-3xl font-medium text-white mb-4">
-                Basic Information
+                {language === "tr"
+                  ? data.basic_info_title_tr
+                  : data.basic_info_title_eng}
               </h3>
               <div className="space-y-4">
                 <div className="flex justify-between">
                   <h4 className="font-medium text-lg text-customGreen">
-                    Birth Date
+                    {language === " tr"
+                      ? data.details.birth_date_tr
+                      : data.details.birth_date_eng}
                   </h4>
-                  <p className="text-white">11.09.1993</p>
+                  <p className="text-white">{data.details.birth_date_value}</p>
                 </div>
                 <div className="flex justify-between">
                   <h4 className="font-medium text-lg text-customGreen">
-                    Residence
+                    {language === "tr"
+                      ? data.details.residence_tr
+                      : data.details.residence_eng}
                   </h4>
-                  <p className="text-white">Ankara</p>
+                  <p className="text-white">{data.details.residence_value}</p>
                 </div>
                 <div className="flex justify-between">
                   <h4 className="font-medium text-lg text-customGreen">
-                    Education
+                    {language === "tr"
+                      ? data.details.education_tr
+                      : data.details.education_eng}
                   </h4>
                   <p className="text-white">
-                    METU BA <br />
-                    Bachelor's Degree, 2017
+                    {language === "tr"
+                      ? data.details.education_value_tr
+                      : data.details.education_value_eng}
                   </p>
                 </div>
                 <div className="flex justify-between">
                   <h4 className="font-medium text-lg text-customGreen">
-                    Preferable Roles
+                    {language === "tr"
+                      ? data.details.roles_tr
+                      : data.details.roles_eng}
                   </h4>
-                  <p className="text-white">Frontend, UI</p>
+                  <p className="text-white">{data.details.roles_value}</p>
                 </div>
               </div>
             </div>
             <div className="flex justify-center items-center flex-1">
               <img
-                src="/images/profile-img.png"
+                src={data.images.profile_image}
                 className="w-full h-full object-cover rounded-[10px] shadow-lg"
               />
             </div>
             <div className="flex flex-col justify-between space-y-4 flex-1">
-              <h3 className="text-3xl font-medium text-white mb-4">About Me</h3>
+              <h3 className="text-3xl font-medium text-white mb-4">
+                {language === "tr"
+                  ? data.about_me.title_tr
+                  : data.about_me.title_eng}
+              </h3>
               <p className="text-white">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam
-                aut, odit laborum aliquam voluptatum nisi mollitia.
+                {language === "tr"
+                  ? data.about_me_text_tr_1
+                  : data.about_me_text_eng_1}
               </p>
               <p className="text-white">
-                Mnima accusamus ratione soluta aperiam sit voluptate? Dicta quod
-                deserunt quam temporibus cumque magnam
+                {language === "tr"
+                  ? data.about_me_text_tr_2
+                  : data.about_me_text_eng_2}
               </p>
             </div>
           </div>
